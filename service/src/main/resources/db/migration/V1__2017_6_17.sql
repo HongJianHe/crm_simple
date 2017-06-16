2d4043fb1f2eb2b8124f29181bb3e9d5d4de6fa8
hongjian.he <hongjian.he@shuyun.com>
2017-06-17 00:40:56 +0800

    添加flyway  注意默认目录和脚本文件名

---
+++ service/src/main/resources/db/migration/V1__2017_6_17.sql
@@ -0,0 +1,8 @@
+-- test
+
+CREATE TABLE `test` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `age` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
